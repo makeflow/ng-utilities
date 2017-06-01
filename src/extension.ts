2588be920bc08954f5c076ae390dbe518dba6592
Lion <maolion.j@gmail.com>
2017-06-01 13:34:29 +0800

    Add open file to the side feature

--- src/extension.ts
+++ src/extension.ts
@@ -21,45 +21,19 @@ export function activate(context: vscode.ExtensionContext) {
     histories[document.fileName] = ++uid;
   });
 
-  let disposable = vscode.commands.registerCommand('ngUtils.switchFilesWithSameName', async () => {
-    let activeTextEditor = vscode.window.activeTextEditor;
-
-    if (!activeTextEditor) {
-      return;
-    }
-
-    let activeDocument = activeTextEditor.document;
-    let filename = activeDocument.fileName;
-
-    let selectItemsPromise = SourceFileManager.resolveSameNameFiles(filename)
-      .then(files => {
-        let items = files.map(file => ({
-          label: file.filename,
-          description: file.kind,
-          file,
-        }));
-
-        items.sort((a, b) => {
-          return histories[a.file.absolutePath] > histories[b.file.absolutePath] ? -1 : 1;
-        });
-
-        return items;
-      });
-
-    let selectItem = await vscode.window.showQuickPick(selectItemsPromise, {
-      placeHolder: 'Select a file',
+  let openCommandDisposable = vscode.commands
+    .registerCommand('ngUtils.openFileWithTheSameName', async () => {
+      openFileWithTheSameName();
     });
 
-    if (!selectItem) {
-      return;
-    }
+  context.subscriptions.push(openCommandDisposable);
 
-    let nextDocument = await vscode.workspace.openTextDocument(selectItem.file.absolutePath);
+  let openToTheSideCommandDisposable = vscode.commands
+    .registerCommand('ngUtils.openFileWithTheSameNameToTheSide', async () => {
+      openFileWithTheSameName(true);
+    });
 
-    vscode.window.showTextDocument(nextDocument, activeTextEditor.viewColumn);
-  });
-
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(openToTheSideCommandDisposable);
 }
 
 export function deactivate() {
@@ -78,4 +52,49 @@ function getSourceFileManager(): SourceFileManager | undefined {
   }
 
   return sourceFileManager;
+}
+
+async function openFileWithTheSameName(side = false) {
+  let activeTextEditor = vscode.window.activeTextEditor;
+
+  if (!activeTextEditor) {
+    return;
+  }
+
+  let activeDocument = activeTextEditor.document;
+  let filename = activeDocument.fileName;
+
+  let selectItemsPromise = SourceFileManager.resolveSameNameFiles(filename)
+    .then(files => {
+      let items = files.map(file => ({
+        label: file.filename,
+        description: file.kind,
+        file,
+      }));
+
+      items.sort((a, b) => {
+        return histories[a.file.absolutePath] > histories[b.file.absolutePath] ? -1 : 1;
+      });
+
+      return items;
+    });
+
+  let selectItem = await vscode.window.showQuickPick(selectItemsPromise, {
+    placeHolder: 'Select a file',
+  });
+
+  if (!selectItem) {
+    return;
+  }
+
+  let nextDocument = await vscode.workspace.openTextDocument(selectItem.file.absolutePath);
+
+  let activeViewColumn = activeTextEditor.viewColumn;
+  let targetViewColumn = activeViewColumn;
+
+  if (side && activeViewColumn) {
+    targetViewColumn = activeViewColumn === 1 ? 2 : activeViewColumn - 1;
+  }
+
+  vscode.window.showTextDocument(nextDocument, targetViewColumn);
 }
